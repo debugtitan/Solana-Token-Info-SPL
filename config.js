823ca833a09ff2a242a1c1849a1fe87da17753ab
debugtitan <alts.devs@gmail.com>
2024-04-04 06:53:37 +0200

    export SPL config settings

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 
 const BOT_TOKEN = process.env.BOT_TOKEN
-const  config = {
+const  SPL_CONFIG = {
     baseUrl: "https://api.mainnet-beta.solana.com"
 }
 
 module.exports = {
-    BOT_TOKEN
+    BOT_TOKEN,
+    SPL_CONFIG
 }
