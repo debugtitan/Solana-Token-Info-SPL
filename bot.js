88c1dfa31e5054ec7837371b220e7faf93e81a39
debugtitan <alts.devs@gmail.com>
2024-04-04 06:53:11 +0200

    added function to query from SPLData class

--- bot.js
+++ bot.js
@@ -1,20 +1,33 @@
 const {Telegraf} = require("telegraf");
-const {message} = require("telegraf/filters");
 const Config = require("./config");
-
+const {SPLData} = require("./actions");
 const bot = new Telegraf(Config.BOT_TOKEN);
 
-bot.command("start", async (ctx) => {
+const SPL = new SPLData();
+
+bot.command("start", async ctx => {
   const firstName = ctx.message.chat.first_name;
-  let msg = `Hi *${firstName}*,\n\nI'm your companion for SPL tokens on the Solana blockchain\n\nUsage:\n/search [token name , symbol or contract]\n\nStay ahead of the game and find detailed information about any "SPL" token`;
-  await ctx.reply(msg,{parse_mode:"Markdown"});
+  let msg = `Hi *${firstName}*,\n\nI'm your companion for SPL tokens on the Solana blockchain\n\nUsage:\n/search [contract]\n\nStay ahead of the game and find detailed information about any "SPL" token`;
+  await ctx.reply(msg, {parse_mode: "Markdown"});
 });
 
-bot.command("search", async (ctx) => {
-    const userMessage = ctx.message.text.split(" ")
-    if (userMessage.length == 2 ){
-        return await ctx.reply(`searching token ${userMessage[1]}`)
+bot.command("search", async ctx => {
+  const userMessage = ctx.message.text.split(" ");
+  if (userMessage.length == 2) {
+    let data = await SPL.GetTokenInfo(userMessage[1]);
+    console.log(data);
+    let tokenInfo = `Name:${data.tokenName} (${data.tokenSymbol})\n\n${data.description}\n\nOwner: _${data.owner}_\n\nDecimal: ${data.decimal}\n`;
+    if (data.tokenLogo) {
+      await ctx.sendPhoto(data.tokenLogo, {
+        caption: `${data.tokenSymbol} Logo`,
+      });
     }
-    await ctx.reply("*Usage*\n/search [token name, symbol or contract]",{parse_mode:"Markdown"})
-})
+    return await ctx.reply(tokenInfo, {
+      parse_mode: "Markdown",
+    });
+  }
+  await ctx.reply("*Usage*\n/search [contract]", {
+    parse_mode: "Markdown",
+  });
+});
 bot.launch();
